6946f8e614b1c7f5fd225edc352ecd01fa2ec4be
GeorgeHY <hanyangkid@163.com>
2014-12-05 23:26:22 +0800

    test TableviewHeaderView
    add tableview and tableview header view

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -8,20 +8,71 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define HeaderViewHeight 200 //头视图的高
+#define ScreenSize self.view.bounds.size //当前屏幕size
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView * tv;
 
 @end
+
+static NSString * cellIdentifier = @"Mycell";
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    UIView * hv = [[UIView alloc]initWithFrame:CGRectMake(0, 0, ScreenSize.width, HeaderViewHeight)];
+//    [hv setBackgroundColor:[UIColor redColor]];
+//    [self.view addSubview:hv];
+    self.tv = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, ScreenSize.width   , ScreenSize.height)];
+    self.tv.delegate = self;
+    self.tv.dataSource = self;
+    self.tv.bounces = NO;
+    self.tv.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:self.tv];
+    [self.tv registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return HeaderViewHeight;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    cell.textLabel.text = @"haha";
+    return cell;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *headerView;
+    if (section == 0) {
+        headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, ScreenSize.width, HeaderViewHeight)];
+        [headerView setBackgroundColor:[UIColor redColor]];
+        return headerView;
+    }
+    
+    return headerView;
+    
+}
+
+
 
 @end
